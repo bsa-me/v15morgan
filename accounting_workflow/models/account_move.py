ce2a686204ca07295061f74340714479c43dce03
Abdel Wahab <abdelwahab.ayoubi@navybits.com>
2022-06-07 12:03:05 +0300

    Refund notification

--- accounting_workflow/models/account_move.py
+++ accounting_workflow/models/account_move.py
@@ -8,3 +8,9 @@ class AccountMove(models.Model):
         template = self.env.ref('accounting_workflow.email_template_invoice_notification')
         template.email_to = self.partner_id.email
         self.env['mail.template'].sudo().browse(template.id).send_mail(self.id)
+
+    def vendor_refund_notification(self):
+        if self.state == 'post':
+            template = self.env.ref('accounting_workflow.refund_notification_automation')
+            template.email_to = self.partner_id.email
+            self.env['mail.template'].sudo().browse(template.id).send_mail(self.id)
