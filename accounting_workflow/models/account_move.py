89942515b7b8ab5deb24e01d8f0ed80ca37b5c74
Abdel Wahab <abdelwahab.ayoubi@navybits.com>
2022-06-07 12:30:28 +0300

    fix bug

--- accounting_workflow/models/account_move.py
+++ accounting_workflow/models/account_move.py
@@ -10,7 +10,7 @@ class AccountMove(models.Model):
         self.env['mail.template'].sudo().browse(template.id).send_mail(self.id)
 
     def vendor_refund_notification(self):
-        if self.state == 'post':
-            template = self.env.ref('accounting_workflow.refund_notification_automation')
+        if self.state == 'post' and self.move_type == 'in_refund':
+            template = self.env.ref('accounting_workflow.email_template_refund_notification')
             template.email_to = self.partner_id.email
             self.env['mail.template'].sudo().browse(template.id).send_mail(self.id)
