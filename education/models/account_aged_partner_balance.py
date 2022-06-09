221bc157251e441abc7857b139949996e44bc0d5
Mostafa Zaydan <mostafazaydan95@gmail.com>
2022-06-10 02:50:25 +0300

    debug

--- education/models/account_aged_partner_balance.py
+++ education/models/account_aged_partner_balance.py
@@ -13,6 +13,7 @@ class ReportAccountAgedPartner(models.AbstractModel):
                 account_move_line.amount_currency as amount_currency,
                 account_move_line.partner_id AS partner_id,
                 partner.name AS partner_name,
+                move.name AS move_name,
                 COALESCE(trust_property.value_text, 'normal') AS partner_trust,
                 COALESCE(account_move_line.currency_id, journal.currency_id) AS report_currency_id,
                 account_move_line.payment_id AS payment_id,
@@ -25,7 +26,7 @@ class ReportAccountAgedPartner(models.AbstractModel):
                 account.code AS account_code,""" + ','.join([("""
                 CASE WHEN period_table.period_index = {i}
                 THEN %(sign)s * ROUND((
-                    account_move_line.balance
+                    account_move_line.balance - COALESCE(SUM(part_debit.amount), 0) + COALESCE(SUM(part_credit.amount), 0)
                 ) * currency_table.rate, currency_table.precision)
                 ELSE 0 END AS period{i}""").format(i=i) for i in range(6)]) + """
             FROM account_move_line
@@ -39,7 +40,16 @@ class ReportAccountAgedPartner(models.AbstractModel):
                 AND trust_property.company_id = account_move_line.company_id
             )
             JOIN {currency_table} ON currency_table.company_id = account_move_line.company_id
-            
+            LEFT JOIN LATERAL (
+                SELECT part.amount, part.debit_move_id
+                FROM account_partial_reconcile part
+                WHERE part.max_date <= %(date)s
+            ) part_debit ON part_debit.debit_move_id = account_move_line.id
+            LEFT JOIN LATERAL (
+                SELECT part.amount, part.credit_move_id
+                FROM account_partial_reconcile part
+                WHERE part.max_date <= %(date)s
+            ) part_credit ON part_credit.credit_move_id = account_move_line.id
             JOIN {period_table} ON (
                 period_table.date_start IS NULL
                 OR COALESCE(account_move_line.date_maturity, account_move_line.date) <= DATE(period_table.date_start)
@@ -48,11 +58,11 @@ class ReportAccountAgedPartner(models.AbstractModel):
                 period_table.date_stop IS NULL
                 OR COALESCE(account_move_line.date_maturity, account_move_line.date) >= DATE(period_table.date_stop)
             )
-            WHERE account.internal_type = %(account_type)s
+            WHERE account.internal_type = %(account_type)s AND move_name like '%SI-%'
             AND account.exclude_from_aged_reports IS NOT TRUE
             GROUP BY account_move_line.id, partner.id, trust_property.id, journal.id, move.id, account.id,
                      period_table.period_index, currency_table.rate, currency_table.precision
-            HAVING ROUND(account_move_line.move_id.amount_residual) != 0
+            HAVING ROUND(account_move_line.balance - COALESCE(SUM(part_debit.amount), 0) + COALESCE(SUM(part_credit.amount), 0), currency_table.precision) != 0
         """).format(
             move_line_fields=self._get_move_line_fields('account_move_line'),
             currency_table=self.env['res.currency']._get_query_currency_table(options),
