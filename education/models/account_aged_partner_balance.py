c4a26e5099ac0df713df43ef3a77c97afd9f06b0
Mostafa Zaydan <mostafazaydan95@gmail.com>
2022-06-10 02:52:51 +0300

    debug

--- education/models/account_aged_partner_balance.py
+++ education/models/account_aged_partner_balance.py
@@ -58,7 +58,7 @@ class ReportAccountAgedPartner(models.AbstractModel):
                 period_table.date_stop IS NULL
                 OR COALESCE(account_move_line.date_maturity, account_move_line.date) >= DATE(period_table.date_stop)
             )
-            WHERE account.internal_type = %(account_type)s AND move_name like '%SI-%'
+            WHERE account.internal_type = %(account_type)s
             AND account.exclude_from_aged_reports IS NOT TRUE
             GROUP BY account_move_line.id, partner.id, trust_property.id, journal.id, move.id, account.id,
                      period_table.period_index, currency_table.rate, currency_table.precision
