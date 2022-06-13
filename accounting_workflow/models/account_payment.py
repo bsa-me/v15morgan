669762b9271b7d3f10b53463f0cf29729974d15b
Abdel Wahab <abdelwahab.ayoubi@navybits.com>
2022-06-13 10:30:07 +0300

    debugging

--- accounting_workflow/models/account_payment.py
+++ accounting_workflow/models/account_payment.py
@@ -4,7 +4,6 @@ from odoo import api, fields, models
 class Payment(models.Model):
     _inherit = 'account.payment'
 
-
     def customer_receipt_notification(self):
         template = self.env.ref('accounting_workflow.email_template_receipt_notification')
         template.email_to = self.partner_id.email
