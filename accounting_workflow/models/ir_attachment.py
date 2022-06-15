2857fa6fc9c396d87b288d615d71dd32be8f3e40
Abdel Wahab <abdelwahab.ayoubi@navybits.com>
2022-06-15 23:26:40 +0300

    Assign country to companies

--- accounting_workflow/models/ir_attachment.py
+++ accounting_workflow/models/ir_attachment.py
@@ -1,6 +1,7 @@
 from odoo import api, fields, models
 import os
 from odoo.exceptions import UserError
+import base64
 
 
 class IrAttachment(models.Model):
@@ -25,3 +26,25 @@ class IrAttachment(models.Model):
         if os.path.isfile(full_path) and not self._same_content(bin_data, full_path):
             raise UserError("path is: " + str(full_path))
         return fname, full_path
+
+    def assign_companies_countries(self):
+        objFile = str(base64.b64decode(self.datas).decode('UTF-8'))
+        reader = objFile.splitlines()
+
+        for row in reader:
+            lines = row.split('\t')
+
+            company = self.env.ref(str(lines[0]))
+            if lines[1]:
+                country_id = self.env['res.country'].search([('name', '=', str(lines[1]))])
+                if country_id:
+                    if len(country_id) > 0:
+                        raise UserError("Multiple results for country " + str(lines[1]))
+                else:
+                    raise UserError(lines[1] + " this country does not exist")
+            if company:
+                company.write({
+                    'country_id': country_id.id,
+                })
+            else:
+                raise UserError("Company of ref " + str(lines[0]) + " does not exist")
